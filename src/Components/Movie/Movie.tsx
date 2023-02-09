fbe868b0616d391309bb31251a1ca8d1b3cd46fa
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-02-09 22:20:28 +0200

    Updates

--- src/Components/Movie/Movie.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-function Movie({}: Props) {
-  return <div>Movie</div>;
-}
-
-export default Movie;
--- src/Components/Performer/Performer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-function Performer({}: Props) {
-  return <div>Performer</div>;
-}
-
-export default Performer;

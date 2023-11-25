cfdc68c0f966f00f02a4015dadd55f65d7f0dd94
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-11-25 15:32:53 +0200

    Changed the name of the main function

--- Client/src/API.ts
+++ Client/src/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "15dcc523002365590c4aab54ede321b0";
-export async function getPopularMovies() {
+export async function getMainMovies() {
   const response = await axios.get(
     `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
   );

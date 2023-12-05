5ef7ded984604d77f2323f9528fe64ca2ebc3762
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2023-12-05 20:17:29 +0200

    Added auth0

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), basicSsl()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [react(), basicSsl()],
+  };
 });

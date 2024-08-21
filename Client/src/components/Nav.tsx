d827517925b7bf37feee274d25810778cf6db833
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-08-21 20:46:22 +0300

    changed the structure of the nav

--- Client/src/components/Nav.tsx
+++ Client/src/components/Nav.tsx
@@ -1,28 +1,12 @@
-import {
-  AiOutlineFire,
-  AiOutlineHome,
-  AiOutlineSearch,
-  AiOutlineUser,
-} from "react-icons/ai";
+import { AiOutlineFire, AiOutlineHome, AiOutlineSearch } from "react-icons/ai";
 
 import styles from "../styles/pages/nav.module.scss";
 import { useNavigate } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-import { getUserId } from "./Utility";
-import axios from "axios";
+
 type Props = {};
 
 function Nav({}: Props) {
-  const { isAuthenticated, user, loginWithRedirect, loginWithPopup } =
-    useAuth0();
   const navigation = useNavigate();
-
-  async function handleLogin() {
-    await loginWithPopup();
-    axios.post("https://localhost3000/user", {
-      userId: getUserId(user),
-    });
-  }
 
   return (
     <div className={styles.nav}>

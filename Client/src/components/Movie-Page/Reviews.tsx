aca5ebda13c08cda8e9ba0fe42255c778b4526a2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-08-21 20:47:14 +0300

    .

--- Client/src/components/Movie-Page/Reviews.tsx
+++ Client/src/components/Movie-Page/Reviews.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-
 type Props = {};
 
 function Reviews({}: Props) {
-  const [reviews, setReviews] = useState(null);
-  return reviews ? (
-    <div>Reviews</div>
-  ) : (
+  return (
     <div>
       <h2>Reviews</h2>
       <h4>There isn't any reviews yet</h4>
